91ead7e5b8ca37a070f0e2e4827ef4adfef0de69
Joseph Hooker <jcolehooker@gmail.com>
2022-03-29 22:06:58 -0500

    set up early basic structure

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+class Book
+    {
+        List<double> grades;
+        public void addGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+    }   
+
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Book = new Book();
+            Book.addGrade(77.2);
+            var x = new double[]{67.3, 99.1, 78.5, 89.4};
+            Console.WriteLine($"Hello {args[0]}!");
+        }
+    }
+
+}
